9988d59055ab6d02f0540e322ccabe24365c5fa8
nhatkycon <nhatkycon@gmail.com>
2014-02-19 22:59:08 +0700

    Remove key

--- web/html/MasterPage.master.cs
+++ web/html/MasterPage.master.cs
@@ -26,9 +26,9 @@ public partial class html_MasterPage : System.Web.UI.MasterPage
         var key = Session["key"];
         if(key == null)
         {
-            Response.ClearContent();
-            Response.Write(string.Format("<h1>Tesing mode</h1>Key is requiered<hr/>Requested IP:{0}<br/>Date:{1}<hr/>{2}", Request.UserHostAddress, DateTime.Now, Request.UserAgent));
-            Response.End();
+            //Response.ClearContent();
+            //Response.Write(string.Format("<h1>Tesing mode</h1>Key is requiered<hr/>Requested IP:{0}<br/>Date:{1}<hr/>{2}", Request.UserHostAddress, DateTime.Now, Request.UserAgent));
+            //Response.End();
         }
     }
 }
